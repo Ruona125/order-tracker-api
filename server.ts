c68be9330992c07e9e2a9f7fde4c4bf85a0f94dc
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-02-21 20:59:23 +0100

    changed the port environmental variable

--- server.ts
+++ server.ts
@@ -1,10 +1,10 @@
 import http from "http";
 import app from "./src/app";
 
-const PORT: number = process.env.PORT ? parseInt(process.env.PORT, 10) : 8000;
+const PORT: number = process.env.SERVER_PORT ? parseInt(process.env.SERVER_PORT, 10) : 8000;
 const server = http.createServer(app);
 
 server.listen(PORT, '0.0.0.0', () => {
-  console.log("listeing to port 8000");
+  console.log(`listeing to port ${PORT}`);
 });
  
