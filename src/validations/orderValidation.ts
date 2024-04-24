9fd4cf4811ca89edd4a3b62a009ea526305a33e4
Ruona125 <ogheneruonaagadagba4@gmail.com>
2024-04-24 22:23:08 +0100

    modified the date validation

--- src/validations/orderValidation.ts
+++ src/validations/orderValidation.ts
@@ -3,8 +3,8 @@ import * as yup from "yup";
 export const createOrderSchema = yup.object({
     details: yup.string(),
     cost_of_order: yup.number(),
-    start_date: yup.date(),
-    end_date: yup.date(),
+    // start_date: yup.date(),
+    // end_date: yup.date(),
     status: yup.string(),
     order_number: yup.number(),
 })
